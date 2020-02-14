25d00121d60cf90acaf445adac26deead637e870
Kevin Walker <jesus.walker3000@gmail.com>
2020-02-13 23:25:00 -0800

    Read user
    Added support to filter by user.id/first_name/last_name, user_emails.email
    and user_phone_number.phone_number.

--- src/models/User.php
+++ src/models/User.php
@@ -66,19 +66,20 @@ class User
 
     public function get()
     {
-        // todo; add pagination
         $query = 'SELECT ' .
             'u.id, u.first_name, u.last_name, u.created_at, u.updated_at,' .
+            'GROUP_CONCAT(DISTINCT ue.email SEPARATOR ", ") as emails, ' .
             'GROUP_CONCAT(DISTINCT ue.email SEPARATOR ", ") as emails, ' .
             'GROUP_CONCAT(DISTINCT upn.phone_number SEPARATOR ", ") as phone_numbers ' .
             'FROM ' . $this->table . ' AS u ' .
             'LEFT JOIN user_emails as ue ON u.id = ue.user_id ' .
-            'LEFT JOIN user_phone_numbers AS upn ON u.id = upn.user_id ';
-        $query .= !is_null($this->id) && !empty($this->id) ? 'WHERE u.id = ? LIMIT 1 ' : 'GROUP BY u.id';
+            'LEFT JOIN user_phone_numbers AS upn ON u.id = upn.user_id ' . $this->resolveFilter();
 
         $stmt = $this->conn->prepare($query);
-        if (!is_null($this->id)) {
-            $stmt->bindParam(1, $this->id);
+
+        $bindValue = $this->resolveBind();
+        if (!is_null($bindValue)) {
+            $stmt->bindParam(1, $bindValue);
         }
         $stmt->execute();
 
@@ -98,9 +99,55 @@ class User
         return $resp;
     }
 
+    private function resolveFilter()
+    {
+        $group = 'GROUP BY u.id';
+        if ($this->isFieldSet($this->id)) {
+            return 'WHERE u.id = ? LIMIT 1 ';
+        }
+        if ($this->isFieldSet($this->first_name)) {
+            return 'WHERE u.first_name = ? ';
+        }
+        if ($this->isFieldSet($this->last_name)) {
+            return 'WHERE u.last_name = ? ';
+        }
+        if ($this->isFieldSet($this->emails)) {
+            return 'WHERE ue.email = ? GROUP BY u.id';
+        }
+        if ($this->isFieldSet($this->phone_numbers)) {
+            return 'WHERE upn.phone_number = ?  GROUP BY u.id';
+        }
+
+        return $group;
+    }
+
+    private function resolveBind()
+    {
+        if ($this->isFieldSet($this->id)) {
+            return $this->id;
+        }
+        if ($this->isFieldSet($this->first_name)) {
+            return $this->first_name;
+        }
+        if ($this->isFieldSet($this->last_name)) {
+            return $this->last_name;
+        }
+        if ($this->isFieldSet($this->emails)) {
+            return $this->emails;
+        }
+        if ($this->isFieldSet($this->phone_numbers)) {
+            return $this->phone_numbers;
+        }
+        return null;
+    }
+
+    private function isFieldSet($field)
+    {
+        return !is_null($field) && !empty($field);
+    }
+
     public function update()
     {
-        // todo; add support to insert emails and phone numbers
         $query = 'UPDATE ' . $this->table . ' SET ' .
             '(first_name, last_name, created_at, updated_at) ' .
             'values (?, ?, NOW(), NOW())';
@@ -109,12 +156,11 @@ class User
         $stmt->bindParam(2, $this->last_name);
         $stmt->execute();
 
-        return $stmt->rowCount() . ' new user was created';
+        return $stmt->rowCount() . ' user updated';
     }
 
     private function log($print)
     {
         die(print_r($print, true));
     }
-
 }
--- src/routes/usersRead.php
+++ src/routes/usersRead.php
@@ -11,6 +11,11 @@ $db = $database->getConnection();
 $user = new User($db);
 
 $user->id = $_GET['id'];
+$user->first_name = $_GET['last_name'];
+$user->last_name = $_GET['last_name'];
+$user->emails = $_GET['email'];
+$user->phone_numbers = $_GET['phone_number'];
+
 $r = $user->get();
 
 $r = $r == false ? ['data' => []] : $r;
